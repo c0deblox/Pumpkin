7d375decf57c557b29d08d8b623f04845da54c47
c0deblox <78987144+c0deblox@users.noreply.github.com>
2021-05-17 17:10:44 -0700

    Create Strat.lua

---
+++ Strat.lua
@@ -0,0 +1,191 @@
+local Strat = Instance.new("Frame")
+local Title = Instance.new("TextLabel")
+local ImageLabel = Instance.new("ImageLabel")
+local Credits = Instance.new("TextLabel")
+local Credits_2 = Instance.new("TextLabel")
+local Credits_3 = Instance.new("TextLabel")
+local Credits_4 = Instance.new("TextLabel")
+local Credits_5 = Instance.new("TextLabel")
+local Credits_6 = Instance.new("TextLabel")
+local Credits_7 = Instance.new("TextLabel")
+
+Strat.Name = "Strat"
+Strat.Parent = game.StarterGui.TB
+Strat.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Strat.BackgroundTransparency = 0.350
+Strat.BorderSizePixel = 0
+Strat.Position = UDim2.new(0.480814427, 0, 0.343672395, 0)
+Strat.Size = UDim2.new(0, 247, 0, 302)
+
+Title.Name = "Title"
+Title.Parent = Strat
+Title.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Title.BackgroundTransparency = 0.350
+Title.BorderSizePixel = 0
+Title.Size = UDim2.new(0, 247, 0, 23)
+Title.Font = Enum.Font.SourceSans
+Title.Text = "              Pumpkin - Tower Battles [Strategy]"
+Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextSize = 14.000
+Title.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Title.TextXAlignment = Enum.TextXAlignment.Left
+
+ImageLabel.Parent = Title
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.0080971662, 0, 0.043478258, 0)
+ImageLabel.Size = UDim2.new(0, 20, 0, 20)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=6816392796"
+
+Credits.Name = "Credits"
+Credits.Parent = Strat
+Credits.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits.BackgroundTransparency = 1.000
+Credits.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits.BorderSizePixel = 0
+Credits.Position = UDim2.new(0.0485829972, 0, 0.0741792396, 0)
+Credits.Size = UDim2.new(0, 235, 0, 23)
+Credits.Font = Enum.Font.SourceSans
+Credits.Text = "Strategy:"
+Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits.TextSize = 14.000
+Credits.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_2.Name = "Credits"
+Credits_2.Parent = Strat
+Credits_2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_2.BackgroundTransparency = 1.000
+Credits_2.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_2.BorderSizePixel = 0
+Credits_2.Position = UDim2.new(0.0485829972, 0, 0.150338173, 0)
+Credits_2.Size = UDim2.new(0, 235, 0, 23)
+Credits_2.Font = Enum.Font.SourceSans
+Credits_2.Text = "Requirements: Must have \"Soldier\" tower"
+Credits_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_2.TextSize = 14.000
+Credits_2.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_2.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_3.Name = "Credits"
+Credits_3.Parent = Strat
+Credits_3.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_3.BackgroundTransparency = 1.000
+Credits_3.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_3.BorderSizePixel = 0
+Credits_3.Position = UDim2.new(0.0485829972, 0, 0.272854745, 0)
+Credits_3.Size = UDim2.new(0, 235, 0, 8)
+Credits_3.Font = Enum.Font.SourceSans
+Credits_3.Text = "enemies come out of. Upgrade that soldier to"
+Credits_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_3.TextSize = 14.000
+Credits_3.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_3.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_4.Name = "Credits"
+Credits_4.Parent = Strat
+Credits_4.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_4.BackgroundTransparency = 1.000
+Credits_4.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_4.BorderSizePixel = 0
+Credits_4.Position = UDim2.new(0.0485829972, 0, 0.301324606, 0)
+Credits_4.Size = UDim2.new(0, 235, 0, 13)
+Credits_4.Font = Enum.Font.SourceSans
+Credits_4.Text = "level 1 then place another soldier. Place it clos-"
+Credits_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_4.TextSize = 14.000
+Credits_4.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_4.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_5.Name = "Credits"
+Credits_5.Parent = Strat
+Credits_5.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_5.BackgroundTransparency = 1.000
+Credits_5.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_5.BorderSizePixel = 0
+Credits_5.Position = UDim2.new(0.0485829972, 0, 0.226497114, 0)
+Credits_5.Size = UDim2.new(0, 235, 0, 14)
+Credits_5.Font = Enum.Font.SourceSans
+Credits_5.Text = "Place a soldier close to the tunnel where the"
+Credits_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_5.TextSize = 14.000
+Credits_5.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_5.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_6.Name = "Credits"
+Credits_6.Parent = Strat
+Credits_6.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_6.BackgroundTransparency = 1.000
+Credits_6.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_6.BorderSizePixel = 0
+Credits_6.Position = UDim2.new(0.0485829972, 0, 0.324503422, 0)
+Credits_6.Size = UDim2.new(0, 235, 0, 19)
+Credits_6.Font = Enum.Font.SourceSans
+Credits_6.Text = "ely to the other soldier then upgrade it to lev-"
+Credits_6.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_6.TextSize = 14.000
+Credits_6.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_6.TextXAlignment = Enum.TextXAlignment.Left
+
+Credits_7.Name = "Credits"
+Credits_7.Parent = Strat
+Credits_7.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Credits_7.BackgroundTransparency = 1.000
+Credits_7.BorderColor3 = Color3.fromRGB(30, 30, 30)
+Credits_7.BorderSizePixel = 0
+Credits_7.Position = UDim2.new(0.0485829972, 0, 0.367549777, 0)
+Credits_7.Size = UDim2.new(0, 235, 0, 19)
+Credits_7.Font = Enum.Font.SourceSans
+Credits_7.Text = "el 1."
+Credits_7.TextColor3 = Color3.fromRGB(255, 255, 255)
+Credits_7.TextSize = 14.000
+Credits_7.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+Credits_7.TextXAlignment = Enum.TextXAlignment.Left
+
+-- Scripts:
+
+local function NCTS_fake_script() -- Strat.Draggable 
+	local script = Instance.new('LocalScript', Strat)
+
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(NCTS_fake_script)()
